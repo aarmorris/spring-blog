661a41dd7726724341269ec8945e06e570ddf480
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-15 14:26:57 -0600

    math controller added

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class HomeController {
     @GetMapping("/")
     @ResponseBody
-    public String helloWorld() {
+    public String landingPage() {
         return "This is the landing page!";
     }
 }
---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,21 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{sum}/and/{sum2}")
+    @ResponseBody
+    public int addInt(@PathVariable int sum, @PathVariable int sum2){
+        return sum + sum2;
+    }
+
+    @GetMapping("/subtract/{sub}/and{sub2}{")
+    @ResponseBody
+    public int subInt(@PathVariable int sub, @PathVariable int sub2){
+        return sub - sub;
+    }
+}
