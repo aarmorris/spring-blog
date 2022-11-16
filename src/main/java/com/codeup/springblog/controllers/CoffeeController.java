37a61952d2a0708c2dd7224acc9df09d5b54a752
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-16 13:41:51 -0600

    creation of pojo

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,10 +1,9 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/coffee")
@@ -15,7 +14,16 @@ public class CoffeeController {
     }
     @GetMapping("/{roast}")
     public String roast(@PathVariable String roast, Model model){
+        Coffee selection = new Coffee();
+        selection.setRoast(roast);
+        selection.setOrigin("Ethiopia");
         model.addAttribute("roast", roast);
         return "coffee";
+    }
+    @PostMapping
+    public String signUp(@RequestParam(name="email") String email, Model model){
+        model.addAttribute("email", email);
+        return "coffee";
+
     }
 }
