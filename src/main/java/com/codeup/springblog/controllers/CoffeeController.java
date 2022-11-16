7140031560d928490245d8f2285c6210d8607d99
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-16 11:22:17 -0600

    coffee-controller

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,7 +1,9 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CoffeeController {
     @GetMapping
     public String coffee(){
+        return "coffee";
+    }
+    @GetMapping("/{roast}")
+    public String roast(@PathVariable String roast, Model model){
+        model.addAttribute("roast", roast);
         return "coffee";
     }
 }
